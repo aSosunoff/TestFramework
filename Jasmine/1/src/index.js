34e02e23a143bf5e1c4ba6f79efb0dddcb3b4bbb
asosunoff <al.sosunoff@gmail.com>
2020-02-16 16:57:24 +0300

    update

--- Jasmine/1/src/index.js
+++ Jasmine/1/src/index.js
@@ -16,8 +16,35 @@ function isNumber(a) {
   return typeof a === 'number';
 }
 
+/**
+ * Factorial
+ * @param {number} n
+ * @returns {number}
+ */
+function factorial(n) {
+  if (!isNumber(n)) { 
+    throw new TypeError('Значение может быть целым числом');
+  }
+
+  if (Number.isNaN(n)) { 
+    throw new TypeError('Значение должно быть числом');
+  }
+
+  if (n === Infinity) { 
+    throw new TypeError('Значение не должно быть бесконечным');
+  }
+
+  let factorialResult = 1;
+
+  while (n) {
+    factorialResult *= n--;
+  }
+	
+  return factorialResult;
+}
 
 module.exports = {
   sum,
-  isNumber
+  isNumber,
+  factorial
 }
--- Jasmine/1/src/index.test.js
+++ Jasmine/1/src/index.test.js
@@ -1,58 +1,112 @@
 const index = require('./index');
  
-describe('0-module-1-task', () => {
-  it('1+1=2', () => {
+describe('index', () => {
+  describe('sum', () => {
+    it('1+1=2', () => {
       expect(index.sum(1, 1)).toEqual(2);
+    });
+
+    it('undefined, undefined', () => {
+      expect(index.sum).toThrowError(TypeError, 'Не число');
+    });
+
+    it('Infinity, Infinity', () => {
+      expect(index.sum(Infinity, Infinity)).toEqual(Infinity);
+    });
+
+    it('" ", " "', () => {
+      const check = () => {
+        index.sum(" ", " ");
+      };
+
+      expect(check).toThrowError(TypeError, 'Не число');
+    });
+
+    it('{}, {}', () => {
+      const check = () => {
+        index.sum({}, {});
+      };
+      
+      expect(check).toThrowError(TypeError, 'Не число');
+    });
+
+    it('[], []', () => {
+      const check = () => {
+        index.sum([], []);
+      };
+      
+      expect(check).toThrowError(TypeError, 'Не число');
+    });
   });
 
-  it('undefined, undefined', () => {
-    expect(index.sum).toThrowError(TypeError, 'Не число');
+  describe('factorial', () => {
+    it('факториал 0 равен 1', () => {
+      expect(index.factorial(0)).toEqual(1);
+    });
+  
+    it('факториал 1 равен 1', () => {
+      expect(index.factorial(1)).toEqual(1);
+    });
+  
+    it('факториал 3 равен 6 ', () => {
+      expect(index.factorial(3)).toEqual(6);
+    });
+  
+    it('факториал 5 равен 120 ', () => {
+      expect(index.factorial(5)).toEqual(120);
+    });
+
+    it('null', () => {
+      const runTaskByNull = () => {
+        index.factorial(null);
+      };
+  
+      expect(runTaskByNull).toThrowError(TypeError, 'Значение может быть целым числом');
+    });
+  
+    it('undefined', () => {
+      const runTaskByUndefined = () => {
+        index.factorial(undefined);
+      };
+  
+      expect(runTaskByUndefined).toThrowError(TypeError, 'Значение может быть целым числом');
+    });
+  
+    it('Infinity', () => {
+      const runTaskByInfinity = () => {
+        index.factorial(Infinity);
+      };
+  
+      expect(runTaskByInfinity).toThrowError(TypeError, 'Значение не должно быть бесконечным');
+    });
+  
+    it('NaN', () => {
+      const runTaskByNaN = () => {
+        index.factorial(NaN);
+      };
+  
+      expect(runTaskByNaN).toThrowError(TypeError, 'Значение должно быть числом');
+    });
   });
-
-  i ('Infinity, Infinity', () => {
-    expect(index.sum(Infinity, Infinity)).toEqual(Infinity);
-  });
-
-  it('" ", " "', () => {
-    const check = () => {
-      index.sum(" ", " ");
-    };
-
-    expect(check).toThrowError(TypeError, 'Не число');
-  });
-
-  it('{}, {}', () => {
-    const check = () => {
-      index.sum({}, {});
-    };
-    
-    expect(check).toThrowError(TypeError, 'Не число');
-  });
-
-  it('[], []', () => {
-    const check = () => {
-      index.sum([], []);
-    };
-    
-    expect(check).toThrowError(TypeError, 'Не число');
+  
+  describe('isNumber', () => {
+    it('', () => {
+      let arrayParameter = [
+        null, 
+        undefined,
+        '',
+        /\s/,
+        [],
+        {},
+        () => {},
+      ];
+  
+      for (let element of arrayParameter) {
+        expect(index.isNumber(element)).toBeFalse();
+      }
+    });
   });
 });
   
-describe('0-module-1-task-isNumber', () => {
-  it('', () => {
-    let arrayParameter = [
-      null, 
-      undefined,
-      '',
-      /\s/,
-      [],
-      {},
-      () => {},
-    ];
 
-    for (let element of arrayParameter) {
-      expect(index.isNumber(element)).toBeFalse();
-    }
-  });
-});
-  
+
